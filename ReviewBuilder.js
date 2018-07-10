17bc49432e2614f0337f81fb451e257c6eeb91b6
Dustin <dustin.tran@live.com>
2018-07-11 00:20:50 +0900

    completed ReviewBuilder.js

--- ReviewBuilder.js
+++ ReviewBuilder.js
@@ -1,7 +1,25 @@
 const fs = require("fs");
 
 class ReviewBuilder {
-  buildReviewsSync() {}
+  constructor() {
+    this.reviews = JSON.parse(fs.readFileSync("./data/reviews.json", "utf8"));
+  }
+
+  buildReviewsSync() {
+    let products = JSON.parse(fs.readFileSync("./data/products.json", "utf8"));
+    let users = JSON.parse(fs.readFileSync("./data/users.json", "utf8"));
+
+    let result = this.reviews.map((x) => {
+      x.productName = products[x.productId - 1].name;
+      x.username = users[x.userId - 1].username;
+      delete x.productId;
+      delete x.userId;
+      return x;
+    });
+
+    console.log(result);
+    return result;
+  }
 }
 
 module.exports = ReviewBuilder;
